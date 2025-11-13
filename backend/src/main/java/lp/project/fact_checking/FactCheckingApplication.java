2c4acdc1fab65cd7e584047a1975cafcf42df3fe
lubor.pesek <lubor.pesek@qinshift.com>
2025-11-13 10:55:29 +0100

    Initial spring boot backend

---
+++ backend/src/main/java/lp/project/fact_checking/FactCheckingApplication.java
@@ -0,0 +1,13 @@
+package lp.project.fact_checking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactCheckingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FactCheckingApplication.class, args);
+    }
+
+}
---
+++ backend/src/main/java/lp/project/fact_checking/ServletInitializer.java
@@ -0,0 +1,13 @@
+package lp.project.fact_checking;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FactCheckingApplication.class);
+	}
+
+}
